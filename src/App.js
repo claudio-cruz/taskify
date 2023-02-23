6a3e29ba57e4fdbe99d78835f9e1443d1ecd9c45
Claudio Cruz <claudiocruz1989@gmail.com>
2023-02-23 15:38:47 +0000

    Rout the navbar links and add customize them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import NavBar from './components/NavBar'; 
 import styles from './App.module.css';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/tasks" render={() => <h1>Tasks</h1>} />
+          <Route exact path="/events" render={() => <h1>Events</h1>} />
+          <Route exact path="/habits" render={() => <h1>Habits</h1>} />
+          <Route exact path="/notes" render={() => <h1>Notes</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
